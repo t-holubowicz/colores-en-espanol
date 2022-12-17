4205835cf1d87088262bd776117d2df9ed71bc1b
Tymoteusz Hołubowicz <tholubowicz27@gmail.com>
2022-12-17 11:49:41 +0100

    Added phonetic pronunciation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const play = (name) => {
-    const path = `assets/mp3/${name}.mp3`;
-    const audio = new Audio(path);
-    audio.play();
-}
+  const path = `assets/mp3/${name}.mp3`;
+  const audio = new Audio(path);
+  audio.play();
+};
